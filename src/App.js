356fa235c5cde0c28f02c6db0e5159f81f92ddbf
valentinpate <valentinp261@gmail.com>
2023-06-25 16:06:08 -0300

    test react terminado

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import logo from './logo.svg';
+import ned from './ned.jpg';
 import './App.css';
 
 function App() {
@@ -7,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Edit <code>src/App.js</code> and save to reload hola hola.
         </p>
         <a
           className="App-link"
@@ -18,6 +19,11 @@ function App() {
           Learn React
         </a>
       </header>
+      <img src={ned} className="Ned" alt="Ned" />
+      <img src={ned} className="Ned" alt="Ned" />
+      <p>
+          Soooo voooo locoooo
+        </p>
     </div>
   );
 }
