520a8e1de1ea6b03ba2dd30ecb75a7dae6f5a9fe
valentinpate <valentinp261@gmail.com>
2023-07-04 15:15:52 -0300

    primeras pruebas modo oscuro

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,19 +1,27 @@
+import React, { useState } from "react";
 import "./Header.css"
+import { LightButton } from "../LightButton/LightButton";
 import {Logo} from "../Logo/Logo"
 import {LogoDark} from "../Logo/LogoDark"
 import {Navbar} from "../Navbar/Navbar"
 
 function Header(){
+    const [modoOscuro, setModoOscuro] = useState(false)
+
+    function oscuro(){
+        setModoOscuro(!modoOscuro)
+    }
+
     return(
         <>
-        <header className="LightHeader">
+        {modoOscuro===false &&<header className="LightHeader">
             <Logo />
             <Navbar />
-        </header>
-        <header className="DarkHeader">
+        </header>}
+        {modoOscuro===true &&<header className="DarkHeader">
             <LogoDark />
             <Navbar />
-        </header>
+        </header>}
         </>
     )
 }
--- src/components/LightButton/LightButton.jsx
+++ src/components/LightButton/LightButton.jsx
@@ -1,15 +1,20 @@
+import React, { useState } from "react";
 import "./LightButton.css"
 
 function LightButton(){
+
+    const [modoOscuro, setModoOscuro] = useState(false)
+
+    function oscuro(){
+        setModoOscuro(!modoOscuro)
+    }
+
     return(
         <div className="CheckboxContainer" id="ButtonContainer">
             <div className="form-check form-switch Light-Mode">
-                <input className="form-check-input" type="checkbox" id="flexSwitchCheckDefault"></input>
-                <label className="form-check-label" for="flexSwitchCheckDefault">Modo oscuro</label>
-            </div>
-            <div className="form-check form-switch Dark-Mode">
-                <input className="form-check-input" type="checkbox" id="flexSwitchCheckDefault"></input>
-                <label className="form-check-label" for="flexSwitchCheckDefault">Modo oscuro</label>
+                <input className="form-check-input" type="checkbox" id="flexSwitchCheckDefault" onClick={oscuro}></input>
+                {modoOscuro === false && <label className="form-check-label" for="flexSwitchCheckDefault">Modo oscuro</label>}
+                {modoOscuro === true && <label className="form-check-label" for="flexSwitchCheckDefault"><p className="Dark-Mode">Modo oscuro</p></label>}
             </div>
         </div>
     )
